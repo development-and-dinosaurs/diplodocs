66aee53a2d8eccb74eea0f5e10e901b0d16a2a08
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2024-04-03 11:42:11 +0100

    Add basic spotless configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "uk.co.developmentanddinosaurs"
@@ -11,6 +12,23 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+spotless {
+    kotlin {
+        target("src/**/*.kt")
+        ktlint()
+    }
+    kotlinGradle {
+        ktlint()
+    }
+    freshmark {
+        target("**/*.md")
+    }
+    yaml {
+        target("**/*.yml")
+        prettier()
+    }
 }
 
 tasks.test {
